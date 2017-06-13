714f181eced1dd85c594e5983b335b3cae6bbaa1
hemu0551 <hemanthkandregula@gmail.com>
2017-06-13 16:41:11 +0530

    update

---
+++ header2.php
@@ -0,0 +1,13 @@
+
+        <section class="section-padding">
+            <div>
+              <div class="text-center">
+                  <h2 class="section-title text-uppercase" style="margin-bottom: 5px;">What we do</h2>
+                  <p class="section-sub">Quisque non erat mi. Etiam congue et augue sed tempus. Aenean sed ipsum luctus, scelerisque</p>
+              </div>
+
+              <div>
+                  <img src="assets/img/banner_php.png" height="200" width="100%">
+              </div>
+              </div>
+        </section>
