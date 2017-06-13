6bc87ecb90e02dba61fd08f0abbcd613d12923db
hemu0551 <hemanthkandregula@gmail.com>
2017-06-13 18:13:15 +0530

    landing page

---
+++ php-development.php
@@ -0,0 +1,41 @@
+<?php include 'header.php'; ?>
+
+
+        <!--page title start-->
+        <section class="page-title ptb-30">
+            <div class="container padding-top-40">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h6 style="float: right;margin-bottom: -5px;">Services</h6>
+                        <ol class="breadcrumb">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Services</a></li>
+                            <li class="active">php development</li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--page title end-->
+
+		<?php include 'header2.php' ?>
+
+              <div class="container padding-top-20">
+                  <div class="col-md-7">
+                      <h2 class="text-bold mb-10">PHP Development</h2>
+                      <p>PHP is being used on 244+ million websites and 2.1 million web servers the world over.</p>
+                      <p>There are several key reasons to consider adopting our PHP development service as an implementation tool for your website. For starters, it is completely dynamic and comes with an extremely large number of libraries, which means more functionality and versatility. Additionally, thanks to its massive popularity, support for PHP development is almost never an issue. So, if you feel the need for a website to front your business in the Internet world, PHP is definitely the way to go.</p>
+                      <p>BigPerl Solutions Pvt Ltd. is a leading PHP development company offering comprehensive range of custom of PHP application development services that steer your business to the next level, streamline processes and enhance growth.</p>
+                      <p>We have successfully leveraged the power of the simplified code of this server side scripting language to develop highly dynamic websites, complex web applications and database-driven websites. Our PHP experts adhere to industry best practices to implement mature PHP solutions that align with our clients’ business strategies.</p>
+                  </div>
+
+                  <div class="col-md-5">
+                      <img src="assets/img/ipad-2.png" class="img-responsive " alt="Image">
+                  </div>
+              </div>
+            </div><!-- /.container -->
+        </section>
+
+        
+
+<?php include 'footer1.php'; ?>
