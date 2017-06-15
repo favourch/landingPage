bf774d6b70fe1a53c607397d0a0e3e5e3d78a018
hemu0551 <hemanthkandregula@gmail.com>
2017-06-15 13:24:05 +0530

    delete

--- header2.php
+++
@@ -1,6 +0,0 @@
-
-        <section class="section-padding">
-              <div>
-                  <img src="assets/img/banner_php.png" height="200" width="100%">
-              </div>
-        </section>
