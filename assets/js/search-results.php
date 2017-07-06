e007d1e7ef395011d032ed122f544e0bb76928f3
hemu0551 <hemanthkandregula@gmail.com>
2017-07-06 08:09:54 +0530

    delete

--- assets/js/search-results.php
+++
@@ -1,5 +0,0 @@
-<div>
-  <h3>{{page}}</h3>
-  <span ng-show="searchText.length">You are searching for "{{searchText}}"</span>
-  <span ng-show="!searchText.length">No search text was entered</span>
-</div>
